c3e9c7f874dda19e125c4bad14fca5522c85c841
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-08-15 10:29:55 +0200

    adding support for arrays

--- view/answer-html_tpl.php
+++ view/answer-html_tpl.php
@@ -44,14 +44,14 @@ Array.prototype.equals = function (array) {
     if (this.length != array.length)
         return false;
 
-    for (var i = 0, l=this.length; i < l; i++) {
+    for (var i = 0, l = this.length; i < l; i++) {
         // Check if we have nested arrays
         if (this[i] instanceof Array && array[i] instanceof Array) {
             // recurse into the nested arrays
             if (!this[i].equals(array[i]))
                 return false;       
         }           
-        else if (this[i] != array[i]) { 
+        else if (this[i] !== array[i]) { 
             // Warning - two different object instances will never be equal: {x:20} != {x:20}
             return false;   
         }           

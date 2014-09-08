bb4d23165d2befbd567bc5e6a64e88b9391c2c01
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-09-08 08:28:28 +0200

    correcting spelling error

--- config/python/lab3.php
+++ config/python/lab3.php
@@ -107,7 +107,7 @@ return [
 [
 
 "text" => "
-<p>Concatinate the two lists [$s1_shortList1Print] and [$s1_shortList2Print]. Answer with your list. 
+<p>Concatenate the two lists [$s1_shortList1Print] and [$s1_shortList2Print]. Answer with your list. 
 </p>
 ",
 

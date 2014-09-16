278d7a4199fb17e69bb6c8dbd8a247503fa298b1
Lew <lewenhagen@live.com>
2014-09-16 13:29:17 +0200

    added sort in instruction

--- config/python/lab3.php
+++ config/python/lab3.php
@@ -342,7 +342,7 @@ return [
 [
 
 "text" => "
-<p>Open the file 'httpd-access.txt' and answer with the first four characters of the eighth element on line 893. Answer as a string.
+<p>Open the file 'httpd-access.txt' and answer with the first four characters of the eighth element on line 893. Use a space (' ') as delimiter and answer as a string.
 </p>
 ",
 
@@ -386,7 +386,7 @@ return [
 [
 
 "text" => "
-<p>Create a function that returns the list passed as argument sorted in numerical and ascending order. Use the list: [$s2_numSerie3Print]. Answer with the sorted list.
+<p>Create a function that returns the list passed as argument sorted in numerical and ascending order. Use the list: [$s2_numSerie3Print], and the built-in method 'sort()'. Answer with the sorted list.
 </p>
 ",
 
@@ -394,6 +394,7 @@ return [
 
     $a = $s2_numSerie3;
     asort($a);
+    
     return implode(",",$a);
 },
 

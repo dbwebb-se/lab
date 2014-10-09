fcfe5f0c7f57e2407bddb41c1c08230dd6fc3a6a
Lew <lewenhagen@live.com>
2014-10-09 21:00:51 +0200

    lab2 initial

---
+++ config/htmlphp/lab2.php
@@ -0,0 +1,230 @@
+<?php
+
+/**
+ * Generate random values to use in lab.
+ */
+include __DIR__ . "/../random.php";
+
+// ################### SECTION 1 ##################
+
+
+
+// ################### SECTION 2 ##################
+
+
+
+// ################### SECTION 3 ##################
+
+
+
+// ################### SECTION 4 ##################
+
+
+
+return [
+
+
+
+/**
+ * Titel and introduction to the lab.
+ */
+"title" => "Lab 2 - Htmlphp",
+
+"intro" => "
+<p>If you need to peek at examples or just want to know more, take a look at the page: http://php.net/manual/en/langref.php. Here you will find everything this lab will go through and much more.
+</p>
+",
+
+
+"sections" => [
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Arrays - with numeric index",
+
+"intro" => "
+<p>??????</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>
+</p>
+",
+
+"answer" => function () {
+
+    return 0;
+},
+
+],
+
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Arrays - with keys",
+
+"intro" => "
+<p>?????
+</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>
+</p>
+",
+
+"answer" => function () {
+
+	return 0;  
+},
+
+],
+
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Arrays - built-in functions",
+
+"intro" => "
+<p>?????
+</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>
+</p>
+",
+
+"answer" => function () {
+		
+	
+	return 0;  
+	
+},
+
+],
+
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Arrays - for-each loop",
+
+"intro" => "
+<p>????? 
+</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>
+</p>
+",
+
+"answer" => function () {
+	
+	return 0;
+},
+
+],
+
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** -----------------------------------------------------------------------------------
+ * Closing up all sections.
+ */
+] // EOF sections
+
+
+
+/**
+ * Closing up this lab.
+ */
+]; // EOF the entire lab

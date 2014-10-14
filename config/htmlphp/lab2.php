2bcd608dc3d330139cb474201ee03a546b7553b3
Lew <lewenhagen@live.com>
2014-10-14 22:21:32 +0200

    more excercises in lab2

--- config/htmlphp/lab2.php
+++ config/htmlphp/lab2.php
@@ -51,11 +51,43 @@ $stateSingle1 = $s1_singleStates[rand_int(0, count($s1_singleStates)-1)];
 // ################### SECTION 2 ##################
 
 $s2_keyArray = array_combine($s1_useThisCountryArray, $s1_useThisCapitalArray);
+$s2_moreKeys = ["Sweden", "Jamaica", "Mexico", "India", "Canada"];
+$s2_moreValues = ["Stockholm", "Kingston", "Mexico City", "New Delhi", "Ottawa"];
+$s2_smallRand = rand_int(0, count($s2_moreKeys)-1);
+$s2_addThisKey = $s2_moreKeys[$s2_smallRand];
+$s2_addThisValue = $s2_moreValues[$s2_smallRand];
 
+$s2_keyArray2 = $s2_keyArray;
+$s2_keyArray2[$s2_addThisKey] = $s2_addThisValue;
+
+$s2_randKey = array_rand($s2_keyArray2, 1);
 
 // ################### SECTION 3 ##################
 
+$s3_arr1 = [
+[123,25,87.55,2,5466],
+[285,11,9.75,9,2216],
+[324,36,20.02,8,4998],
+[499,98,21.63,5,9855],
+[522,87,54.98,3,6533]
+];
+$s3_arrWhich = rand_int(1, count($s3_arr1)-1);
+$s3_arrSel = $s3_arr1[$s3_arrWhich];
+$s3_arrSelPrint = implode(",", $s3_arrSel);
+$s3_smallRand2 = rand_int(0, count($s3_arrSel)-1);
+$s3_arrSel2 = $s3_arr1[$s3_arrWhich-1];
+$s3_arrSel2Print = implode(",", $s3_arrSel2);
 
+$s3_singleNumbers = [3, 56, 65, 555, 122, 45, 669, 55.89, 7, 1];
+$s3_single = $s3_singleNumbers[rand_int(0, count($s3_singleNumbers)-1)];
+
+$s3_newArray = $s1_useThisCountryArray;
+//$s3_shiftedArray = array_shift($s3_newArray);
+
+$s3_singleStates = ["Washington", "Wyoming", "Oregon", "Montana", "North Dakota"];
+$s3_stateRand = rand_int(1, count($s3_singleStates)-1);
+$s3_singelState1 = $s3_singleStates[$s3_stateRand];
+$s3_singleState2 = $s3_singleStates[$s3_stateRand-1];
 
 // ################### SECTION 4 ##################
 
@@ -241,6 +273,45 @@ return [
 
 
 
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Add the key '$s2_addThisKey' with the value '$s2_addThisValue' to your 'keyArray'. Answer with the updated array.
+</p>
+",
+
+"answer" => function () use($s2_keyArray2) {
+	
+	
+	
+	return $s2_keyArray2;  
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Answer with the value for the key '$s2_randKey'.
+</p>
+",
+
+"answer" => function () use($s2_keyArray2, $s2_randKey) {
+	return $s2_keyArray2[$s2_randKey];  
+},
+
+],
+
+
+
 /**
  * Closing up this section.
  */
@@ -272,15 +343,164 @@ return [
 [
 
 "text" => "
-<p>
+<p>Find the number of items in the array [$s3_arrSelPrint]. Answer with the result as an integer.
 </p>
 ",
 
-"answer" => function () {
+"answer" => function () use ($s3_arrSel) {
 		
 	
-	return 0;  
+	return count($s3_arrSel);  
 	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Sort the array [$s3_arrSel2Print] in ascending order. Make sure that it doesn't maintain the key association. Answer with the sorted array.
+</p>
+",
+
+"answer" => function () use ($s3_arrSel2) {
+		
+	sort($s3_arrSel2);
+
+	return $s3_arrSel2;  
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Sort the array [$s3_arrSelPrint] in decending order. Make sure that it does maintain the key association. Answer with the sorted array.
+</p>
+",
+
+"answer" => function () use ($s3_arrSel) {
+		
+	asort($s3_arrSel);
+
+	return $s3_arrSel;  
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Use pop() on the array [$s3_arrSelPrint] and answer with the popped item.
+</p>
+",
+
+"answer" => function () use ($s3_arrSel) {
+		
+	$temp = array_pop($s3_arrSel);
+
+	return $temp;  
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Use push() on the array [$s3_arrSel2Print] and insert the number $s3_single. Answer with the resulting array.
+</p>
+",
+
+"answer" => function () use ($s3_arrSel2, $s3_single) {
+		
+	array_push($s3_arrSel2, $s3_single);
+
+	return $s3_arrSel2;  
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Copy your array 'countries' to a new array called 'newArray'. Use shift() on the new array and answer with the shifted item.
+</p>
+",
+
+"answer" => function () use ($s3_newArray) {
+
+	return array_shift($s3_newArray);  
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Use unshift() on your 'newArray' add the items '$s3_singelState1' and '$s3_singleState2' in the beginning of the array. Answer with the modified array.
+</p>
+",
+
+"answer" => function () use ($s3_newArray, $s3_singelState1, $s3_singleState2) {
+
+	array_shift($s3_newArray);
+	array_unshift($s3_newArray, $s3_singelState1, $s3_singleState2);
+
+	return $s3_newArray;   
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+<p>Reverse the order of the array [$s3_arrSelPrint]. Answer with the modified array.
+</p>
+",
+
+"answer" => function () use ($s3_arrSel) {
+
+	return array_reverse($s3_arrSel);   
 },
 
 ],

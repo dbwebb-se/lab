2e2fc1f31d9451326b8eabbaafa8756362ab3303
U-AD\klw <klw@BTHL0695.ad.bth.se>
2015-12-07 11:14:09 +0100

    started on oophp lab1

--- config/oophp/lab1.php
+++ config/oophp/lab1.php
@@ -5,6 +5,48 @@
  */
 include __DIR__ . "/../random.php";
 
+// CARD ATTRIBUTES
+$allValues = [
+    "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"
+];
+$allSuits = [
+    "clubs", "diamonds", "spades", "hearts"
+];
+
+// SECTION 1
+$randValue = rand_int(0, (count($allValues)-1));
+$randSuit = rand_int(0, (count($allSuits)-1));
+
+$randValue2 = rand_int(0, (count($allValues)-1));
+$randSuit2 = rand_int(0, (count($allSuits)-1));
+
+$useThisValue = $allValues[$randValue];
+$useThisSuit = $allSuits[$randSuit];
+
+$useThisValue2 = $allValues[$randValue2];
+$useThisSuit2 = $allSuits[$randSuit2];
+
+
+class Card {
+    private $value = "";
+    private $suit = "";
+
+    public function __construct($val, $suit) {
+        $this->value = $val;
+        $this->suit = $suit;
+    }
+
+    public function getValue() {
+        return $this->value;
+    }
+
+    public function getCard() {
+        return $this->value . " of " . $this->suit;
+    }
+}
+
+$myCard = new Card($useThisValue, $useThisSuit);
+
 
 return [
 
@@ -32,7 +74,7 @@ return [
 "title" => "Classes and Objects",
 
 "intro" => "
-<p>TBD.</p>
+<p>Create a Card-class.</p>
 ",
 
 "shuffle" => false,
@@ -42,18 +84,99 @@ return [
 
 
 /** ---------------------------------------------------------------------------
- * A question.
+ * A question. 1.1
  */
 [
 
 "text" => "
-<p>TBD.
+<p>Create a class called 'Card'. Create a private member called 'value' which holds the string-value: '$useThisValue'. Create a public function called 'getValue()' that returns the member 'value'. Create an instance of the class and call it 'card1'. Answer with a call to: '&#36;card1-&gt;getValue()'.
 </p>
 ",
 
-"answer" => function () {
+"answer" => function () use ($myCard){
 
-    return true;
+    return $myCard->getValue();
+},
+
+],
+
+
+
+/** ---------------------------------------------------------------------------
+ * A question. 1.2
+ */
+[
+
+"text" => "
+<p>Continue with the class 'Card'. Create another private member called 'suit' which holds the string-value: '$useThisSuit'. Create a public function called 'getCard()' that returns a string in the format: '$useThisValue of $useThisSuit'. Create an instance of the class and call it 'card2'. Answer with a call to: '&#36;card2-&gt;getCard()'.
+</p>
+",
+
+"answer" => function () use ($myCard){
+
+    return $myCard->getCard();
+},
+
+],
+
+
+
+/** ---------------------------------------------------------------------------
+ * A question. 1.3
+ */
+[
+
+"text" => "
+<p>Continue with the class 'Card'. Create a constructor that takes two arguments, value and suit. Set the private members and answer with a call to: '&#36;card2-&gt;getCard()'.
+</p>
+",
+
+"answer" => function () use ($myCard){
+
+    return $myCard->getCard();
+},
+
+],
+
+
+
+/** ---------------------------------------------------------------------------
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===========================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Classes and Objects",
+
+"intro" => "
+<p>Create a deck of cards.</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** ---------------------------------------------------------------------------
+ * A question. 1.1
+ */
+[
+
+"text" => "
+<p>Create a class called 'Deck'.
+</p>
+",
+
+"answer" => function () use ($myDeck){
+
+    return $myDeck->getCard();
 },
 
 ],

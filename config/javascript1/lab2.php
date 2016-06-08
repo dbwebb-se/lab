c599e73880ff7b14fab6118db96a0cad69d2f1f5
Kenneth Lewenhagen <lewenhagen@live.com>
2016-06-08 11:19:21 +0200

    changed to EOD

--- config/javascript1/lab2.php
+++ config/javascript1/lab2.php
@@ -92,10 +92,10 @@ return [
  */
 "title" => "Lab 2 - javascript1",
 
-"intro" => "
-<p>Practice to write functions.
-</p>
-",
+"intro" => <<<EOD
+Practice to write functions.
+EOD
+,
 
 
 "sections" => [
@@ -108,7 +108,10 @@ return [
 [
 "title" => "Basic functions",
 
-"intro" => "",
+"intro" => <<<EOD
+Practice on basic functions.
+EOD
+,
 
 "shuffle" => false,
 
@@ -117,13 +120,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.1.
  */
 [
 
-"text" => "
-<p>Create a function called 'sumNumbers()'. The function should take two arguments and return the sum of them. Test the function using the arguments $num1 and $num2. Answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function called 'sumNumbers()'. The function should take two arguments and return the sum of them. Test the function using the arguments $num1 and $num2.  
+
+Answer with the result.
+EOD
+,
 
 "answer" => function () use ($num1, $num2) {
 
@@ -135,13 +141,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.2.
  */
 [
 
-"text" => "
-<p>Create a function called 'productNumbers()'. The function should take three arguments and return the product of them. Try the function using the numbers $num1, $num2 and $num3. Answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function called 'productNumbers()'. The function should take three arguments and return the product of them. Try the function using the numbers $num1, $num2 and $num3.  
+
+Answer with the result.
+EOD
+,
 
 "answer" => function () use ($num1, $num2, $num3) {
     
@@ -153,13 +162,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.3.
  */
 [
 
-"text" => "
-<p>Create a function 'sumRangeNumbers()' that returns the sum of all numbers between two chosen numbers. The function should take two arguments, one representing the lowest boundary and one that represents the highest boundary. For example, the arguments 10 and 20 should return the sum of 10+11+12+13...+20. Test it using the values $range1 and $range2 and answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function 'sumRangeNumbers()' that returns the sum of all numbers between two chosen numbers. The function should take two arguments, one representing the lowest boundary and one that represents the highest boundary. For example, the arguments 10 and 20 should return the sum of 10+11+12+13...+20.  
+
+Test it using the values $range1 and $range2 and answer with the result.
+EOD
+,
 
 "answer" => function () use ($range1, $range2) {
 
@@ -175,13 +187,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.4.
  */
 [
 
-"text" => "
-<p>Create a function 'stringPhrase()' that returns a phrase. Your word is '$color'. Pass the word as an argument to the function and the returned phrase should be: 'My favorite color is $color.'. Test your function and answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function 'stringPhrase()' that returns a phrase. Your word is '$color'. Pass the word as an argument to the function and the returned phrase should be: 'My favorite color is $color.'.  
+
+Test your function and answer with the result.
+EOD
+,
 
 "answer" => function () use ($color) {
 
@@ -221,13 +236,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.5.
  */
 [
 
-"text" => "
-<p>Create a function called 'fruitColor()' that takes one argument called 'fruit' and returns the color of the fruit. The function should be aware of the following fruits ($fruitString) with respective color ($fruitColorString). Try it out using the fruit '$fruit' and answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function called 'fruitColor()' that takes one argument called 'fruit' and returns the color of the fruit. The function should be aware of the following fruits ($fruitString) with respective color ($fruitColorString).  
+
+Try it out using the fruit '$fruit' and answer with the result.
+EOD
+,
 
 "answer" => function () use ($fruitColor) {
 
@@ -241,13 +259,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.6.
  */
 [
 
-"text" => "
-<p>Create a function 'printRange()' that takes two arguments 'rangeStart' and 'rangeStop' and returns a string with all numbers comma-separated in the range. Try it using the arguments $range3 and $range4. Answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function 'printRange()' that takes two arguments 'rangeStart' and 'rangeStop' and returns a string with all numbers comma-separated in the range. Try it using the arguments $range3 and $range4.  
+
+Answer with the result.
+EOD
+,
 
 "answer" => function () use ($range3, $range4) {
     
@@ -262,13 +283,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.7.
  */
 [
 
-"text" => "
-<p>Create a function 'printRangeReversed()' that takes two arguments 'rangeStart' and 'rangeStop' and returns a string with all numbers comma-separated in the range. Try it using the arguments $range4 and $range3. Answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function 'printRangeReversed()' that takes two arguments 'rangeStart' and 'rangeStop' and returns a string with all numbers comma-separated in the range. Try it using the arguments $range4 and $range3.  
+
+Answer with the result.
+EOD
+,
 
 "answer" => function () use ($range3, $range4) {
     
@@ -283,13 +307,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.8.
  */
 [
 
-"text" => "
-<p>Create a function 'printAnyRange()' that takes two arguments 'rangeStart' and 'rangeStop' and returns a string with all numbers comma-separated in the range. If 'rangeStart' is smaller than 'rangeStop' then call the function 'printRange()'.  If 'rangeStart' is greater than 'rangeStop' the call the function 'printRangeReversed()'. Try it using the arguments $range3 and $range4 (both ways). Answer with the result using arguments $range3 and $range4.
-</p>",
+"text" => <<<EOD
+Create a function 'printAnyRange()' that takes two arguments 'rangeStart' and 'rangeStop' and returns a string with all numbers comma-separated in the range. If 'rangeStart' is smaller than 'rangeStop' then call the function 'printRange()'.  If 'rangeStart' is greater than 'rangeStop' the call the function 'printRangeReversed()'. Try it using the arguments $range3 and $range4 (both ways).  
+
+Answer with the result using arguments $range3 and $range4.
+EOD
+,
 
 "answer" => function () use ($range3, $range4) {
     
@@ -347,14 +374,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.9.
  */
 [
 
-"text" => "
-<p>Create a function called 'stringRepeat()' that returns a string a specific number of times. The function should take two arguments, one string and one number: '$currWord' and $smallRand2. The number represents the number of times the string should be added to a string. Test the function and answer with the result.
-</p>
-",
+"text" => <<<EOD
+Create a function called 'stringRepeat()' that returns a string a specific number of times. The function should take two arguments, one string and one number: '$currWord' and $smallRand2. The number represents the number of times the string should be added to a string.  
+
+Test the function and answer with the result.
+EOD
+,
 
 "answer" => function () use ($currWord, $smallRand2) {
     
@@ -371,14 +400,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.10.
  */
 [
 
-"text" => "
-<p>Create a function called 'calculateInterest()' that returns the money you have after x years of interest, given three arguments: $money, $years and $interest. First argument represents the start money, the second argument represents the number of years your money produces interest. The third argument is the interest rate in percent (%). Test your function and answer with the result with a maximum of 4 decimals. 
-</p>
-",
+"text" => <<<EOD
+Create a function called 'calculateInterest()' that returns the money you have after x years of interest, given three arguments: $money, $years and $interest. First argument represents the start money, the second argument represents the number of years your money produces interest. The third argument is the interest rate in percent (%).  
+
+Test your function and answer with the result with a maximum of 4 decimals.
+EOD
+,
 
 "answer" => function () use ($money, $years, $interest) {
    
@@ -397,13 +428,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.11.
  */
 [
 
-"text" => "
-<p>Create a function 'inRange()' that takes three arguments, 'rangeStart', 'rangeStop' and 'value'. The function should return boolean 'true' if 'value' is greater than 'rangeStart' and less than 'rangeStop'. Otherwise it should return boolean 'false'. Try it out using the range $range5 - $range6 and the value $inRange. Answer with the result.
-</p>",
+"text" => <<<EOD
+Create a function 'inRange()' that takes three arguments, 'rangeStart', 'rangeStop' and 'value'. The function should return boolean 'true' if 'value' is greater than 'rangeStart' and less than 'rangeStop'. Otherwise it should return boolean 'false'. Try it out using the range $range5 - $range6 and the value $inRange.  
+
+Answer with the result.
+EOD
+,
 
 "answer" => function () use ($range5, $range6, $inRange) {
     
@@ -420,13 +454,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.12.
  */
 [
 
-"text" => "
-<p>Try out the function 'inRange()' using the range $range5 - $range6 and the value $outRange. Answer with the result.
-</p>",
+"text" => <<<EOD
+Try out the function 'inRange()' using the range $range5 - $range6 and the value $outRange.  
+
+Answer with the result.
+EOD
+,
 
 "answer" => function () use ($range5, $range6, $outRange) {
     
@@ -443,13 +480,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.13.
  */
 [
 
-"text" => "
-<p>Create a function called 'degreesToRadians()' that should take one argument, a degree value. The function should convert the value to radians and return the result with max 4 decimals. Test your function with the value $degree and answer with the result.  
-</p>",
+"text" => <<<EOD
+Create a function called 'degreesToRadians()' that should take one argument, a degree value. The function should convert the value to radians and return the result with max 4 decimals.  
+
+Test your function with the value $degree and answer with the result.
+EOD
+,
 
 "answer" => function () use ($degree) {
     
@@ -461,13 +501,16 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 1.14.
  */
 [
 
-"text" => "
-<p>Create a function called 'fizzBuzz()' that takes two arguments 'start' and 'stop' and returns a comma-separated string. The arguments represents the starting point and stop point of the game 'Fizz Buzz' (http://en.wikipedia.org/wiki/Fizz_buzz). The function should run from start to stop and add 'Fizz', 'Buzz' or both to your 'result'-variable at appropriate numbers. Each entry to your result should be comma-separated. If 'stop' is equal or lower than 'start', the function should return an appropriate error message. Test the function using start=$fbStart and stop=$fbStop.
-</p>",
+"text" => <<<EOD
+Create a function called 'fizzBuzz()' that takes two arguments 'start' and 'stop' and returns a comma-separated string. The arguments represents the starting point and stop point of the game 'Fizz Buzz' (http://en.wikipedia.org/wiki/Fizz_buzz). The function should run from start to stop and add 'Fizz', 'Buzz' or both to your 'result'-variable at appropriate numbers. Each entry to your result should be comma-separated. If 'stop' is equal or lower than 'start', the function should return an appropriate error message.  
+
+Test the function using start=$fbStart and stop=$fbStop.
+EOD
+,
 
 "answer" => function () use ($fbStart, $fbStop) {
     
@@ -516,10 +559,10 @@ return [
 [
 "title" => "Black jack functions",
 
-"intro" => "
-<p>In this section, you could re-use your code from lab 1
-</p>
-",
+"intro" => <<<EOD
+In this section, you could re-use your code from lab 1
+EOD
+,
 
 "shuffle" => false,
 
@@ -528,15 +571,17 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 2.1.
  */
 
 [
 
-"text" => "
-<p>Create a function called 'printSum()' that should take two variables, the sum of the players hand and the sum of the dealers hand. Your hand should be three cards with the values: $card1, $card2 and $card3. The dealers hand should be three card with the values: $dealer1, $dealer2, $dealer3. The function should return the sum and the player: 'Player: $cardSum, Dealer: $dealerSum'. Test your function with the given values and answer with the result.
-</p>
-",
+"text" => <<<EOD
+Create a function called 'printSum()' that should take two variables, the sum of the players hand and the sum of the dealers hand. Your hand should be three cards with the values: $card1, $card2 and $card3. The dealers hand should be three card with the values: $dealer1, $dealer2, $dealer3. The function should return the sum and the player: 'Player: $cardSum, Dealer: $dealerSum'.  
+
+Test your function with the given values and answer with the result.
+EOD
+,
 
 "answer" => function () use ($cardSum, $dealerSum) {
 
@@ -548,15 +593,17 @@ return [
 
 
 /** -----------------------------------------------------------------------------------
- * A question.
+ * A question, 2.2.
  */
 
 [
 
-"text" => "
-<p>Create a function called 'printResult()' that should take two variables, the sum of the players hand and the sum of the dealers hand. Players hand should be three cards with the values: $card1, $card2 and $card3. The dealers hand should be three card with the values: $dealer1, $dealer2, $dealer3. This time you should include the check from lab 1 where you find out the boundaries of the player and the dealer. Player hand = 21 ('black jack'), Player hand less than 21 ('safe'), Player hand larger than 21 ('busted'). Dealer hand less than 17 ('safe'), Dealer hand larger or equal to 17 and less than 21 ('stop'), Dealer hand = 21 ('black jack'), Delaer hand larger than 21 ('busted'). Return a string in the format: 'Player: safe, Dealer: busted'. Test your function with the given values and answer with the result.
-</p>
-",
+"text" => <<<EOD
+Create a function called 'printResult()' that should take two variables, the sum of the players hand and the sum of the dealers hand. Players hand should be three cards with the values: $card1, $card2 and $card3. The dealers hand should be three card with the values: $dealer1, $dealer2, $dealer3. This time you should include the check from lab 1 where you find out the boundaries of the player and the dealer. Player hand = 21 ('black jack'), Player hand less than 21 ('safe'), Player hand larger than 21 ('busted'). Dealer hand less than 17 ('safe'), Dealer hand larger or equal to 17 and less than 21 ('stop'), Dealer hand = 21 ('black jack'), Delaer hand larger than 21 ('busted'). Return a string in the format: 'Player: safe, Dealer: busted'.  
+
+Test your function with the given values and answer with the result.
+EOD
+,
 
 "answer" => function () use ($cardSum, $dealerSum) {
 

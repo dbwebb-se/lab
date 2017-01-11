612a5ad0a13f3185b5dac28937f2bc74a0bf9b6e
Andreas Arnesson <Andreas_osby@hotmail.com>
2017-01-11 11:28:28 +0100

    lab2 oopython: fixed spelling

--- config/oopython/lab2.php
+++ config/oopython/lab2.php
@@ -8,7 +8,7 @@ include __DIR__ . "/../random.php";
 //SECTION 1 ****************************************************
 
 $s1_present         = ["Ozelot", "Kvothe", "It's a very, very, merry, merry christmas. Gonna party on 'til Santa grants my wishes.", rand_int(10000, 15000)];
-$s1_christPresent   = ["Pirate", "Zeldah", "You, oh ,oh, a Christmas. My Christmas tree is delicious", rand_int(20000, 25000)];
+$s1_christPresent   = ["Pirate", "Zeldah", "You, oh, oh, a Christmas. My Christmas tree is delicious", rand_int(20000, 25000)];
 $s1_compPresent     = ["Icecream", "Lew", "That's why I celebrate Christmas 'Cause this overweighted redneck devil is big business", rand_int(1, 20)];
 $s1_searchStrings   = [
                     ["ew", "False False True "],
@@ -28,7 +28,7 @@ $s1_month2          = $s1_listMonth[rand_int(1, count($s1_listMonth)-1)];
 $s1_day2            = $s1_listDay[rand_int(1, count($s1_listDay)-1)];
 
 $s1_dogNames        = ["Buster", "James", "Zimba", "Goliat"];
-$s1_dogRaces        = ["Shitzu", "Cocker spaniel", "Rottwiler", "Grand danois"];
+$s1_dogRaces        = ["Shitzu", "Cocker spaniel", "Rottweiler", "Grand danois"];
 $s1_dogSizes        = ["small", "medium", "big", "big"];
 $s1_dogDays         = [rand_int(5, 10), rand_int(7, 14), rand_int(5, 15), rand_int(20, 25)];
 #$s1_dogOrders       = [
@@ -179,7 +179,7 @@ EOD
 "text" => <<<EOD
 The next class that you will create will hold dogs of a certain type. Eg. small, medium or big sized.
 
-Create a new class called DogType. In the constructor declare the variables `size`, `cost` (per day), `numberOfAllowedDogs` and a private list that will contain *Dog objects*. Declare the list with `Dogs` to an empty list in the constructor.  
+Create a new class called DogType. In the constructor declare the variables `size`, `cost` (per day), `numberOfAllowedDogs` and a private list that will contain *Dog objects*. Declare the list containing `Dogs` to an empty list in the constructor.  
 Give the class a new method called `addDog`, it should take a Dog object as parameter. In the method add the Dog to the list if you have room for it and it has the correct size. If you add the dog return `True` otherwise return `False`.
 
 Initialize a new DogType variable in the code below, name it `smallDogs`, set `size` to "small", `cost` to `$s1_priceSmall` and `numberOfAllowedDogs` to `3`. 
@@ -225,13 +225,13 @@ EOD
 [
 
 "text" => <<<EOD
-Time to make the Kennel class. Chose a way to store three *DogType objects*, a list, a dictionary or one variable for each. We will have the dog types `small`, `medium` and `big` dogs.  
+Time to make the Kennel class. Choose a way to store three *DogType objects*, a list, a dictionary or one variable for each. We will have the dog types `small`, `medium` and `big` dogs.  
 In the constructor:  
 Initialize the DogType for small dogs with size "small", cost `$s1_priceSmall` and numberOfAllowedDogs `3`.  
 Initialize the DogType for medium dogs with size `medium`, cost `$s1_priceMedium` and numberOfAllowedDogs `2`.  
 Initialize the DogType for big dogs with size `big`, cost `$s1_priceBig` and numberOfAllowedDogs `1`.
 
-Create a method in Kennel that checks-in a new dog at the Kennel, it takes a *Dog object* as parameter. In the method add the dog to the appropriate DogType and reutrn whats returned from the DogType `addDog` method.
+Create a method in Kennel that checks-in a new dog at the Kennel, it takes a *Dog object* as parameter. In the method add the dog to the appropriate DogType and return whats returned from the DogType `addDog` method.
 
 In the code below initialize a new Kennel variable and check-in `dog1` to the kennel.
 
@@ -253,13 +253,13 @@ EOD
 [
 
 "text" => <<<EOD
-Create a new method in the kennel class for checking-out a Dog. It takes a `name` and `size` as parameter.  
-In the method try to retrieve the dog from apropriate DogType with the DogType method `retrieveDog`. The Checkout method should return the `cost` for having the dog at the Kennel, aka days stayed * cost of size.
+Create a new method in the kennel class for checking out a Dog. It takes a `name` and `size` as parameter.  
+In the method try to retrieve the dog from appropriate DogType with the DogType method `retrieveDog`. The check out method should return the `cost` for having the dog at the Kennel, aka days stayed * cost of size.
 
 In the code below create a new Dog variable called `dog2`, give it the name "$s1_dog2[0]", the size "$s1_dog2[1]", the race "$s1_dog2[2]" and `$s1_dog2[3]` days to stay.  
-Check-in `dog2` at the kennel and Checkout `dog1`, "$s1_dog1[0]", "$s1_dog1[1]", from the Kennel.
+Check-in `dog2` at the kennel and checkout `dog1`, "$s1_dog1[0]", "$s1_dog1[1]", from the Kennel.
 
-Answer with the price of having `dog1` at the Kennel, whats returned from the check-out method.
+Answer with the price of having `dog1` at the Kennel, the result from the check out method.
 EOD
 ,
 
@@ -309,6 +309,8 @@ Give ChristmasPresent the variale `rhyme`. Use *super* to initiate the variables
 Override the `info` method from Present to return "content: xxx, recipient: yyy, rhyme: zzz". A tip, you can access the parent method with super and just add rhyme to the returned string and return that.
 
 Create a new ChristmasPresent variable in the code below. Give it the content "$s1_christPresent[0]", recipient "$s1_christPresent[1]" and rhyme "$s1_christPresent[2]".
+
+Answer with the christmas presents `info` method.
 EOD
 ,
 

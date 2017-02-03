978d538d7db08d5565112e4816ae71ec378b14f0
Andreas Arnesson <Andreas_osby@hotmail.com>
2017-02-03 11:12:50 +0100

    oopython lab3: added points and fixed validation

--- config/oopython/lab3.php
+++ config/oopython/lab3.php
@@ -28,15 +28,19 @@ EOD
 ,
 
 "header" => <<<EOD
+# pylint: disable=line-too-long
 # pylint: disable=unused-import
 import re
 # pylint: enable=unused-import
 #
-textfile = open('ircLog.txt', 'r')
-ircLog = textfile.read()
-textfile.close()
+TEXTFILE = open('ircLog.txt', 'r')
+IRCLOG = TEXTFILE.read()
+TEXTFILE.close()
 EOD
 ,
+
+"passPercentage" => 80/100,
+"passDistinctPercentage" => 100/100,
 
 "sections" => [
 
@@ -51,7 +55,7 @@ EOD
 "intro" => <<<EOD
 
 The lab consists of three sections. In section one you will practice regex on strings. In section two you will practice regex on the content from a file, the irc log. In section three you will practice on replacing text in strings.
-In section two, use your patterns on the variable `ircLog`.
+In section two, use your patterns on the variable `IRCLOG`.
 
 Answer with the result from the re functions unless specified differently in the assignment.
 EOD
@@ -76,6 +80,8 @@ Use the re modules findall function.
 EOD
 ,
 
+"points" => 1,
+
 "answer" => function () use ($s1_twisterWord) {
 
     $result = [$s1_twisterWord];
@@ -97,6 +103,8 @@ Write a pattern that only matches the words that start with a *big letter* in th
 Use the re modules findall function.
 EOD
 ,
+
+"points" => 1,
 
 "answer" => function () {
 
@@ -120,6 +128,8 @@ Use the re modules findall function.
 EOD
 ,
 
+"points" => 1,
+
 "answer" => function ()  {
 
     return ['123', '563', '726'];
@@ -142,6 +152,8 @@ Write a pattern that matches the characters `()[] and {}` and `the words inside`
 Use the re modules findall function.
 EOD
 ,
+
+"points" => 1,
 
 "answer" => function () {
 
@@ -168,6 +180,8 @@ Use the re modules findall function. Convert the result to a string and answer w
 EOD
 ,
 
+"points" => 2,
+
 "answer" => function () {
 
 	return "[('https', 'dbwebb.se', ''), ('ftp', 'bth.com', '32'), ('file', 'localhost', '8585'), ('http', 'v2-dbwebb.se', '')]";
@@ -190,7 +204,7 @@ EOD
 "title" => "Regex on content from a file",
 
 "intro" => <<<EOD
-Use the re modules findall function with the variable `ircLog`.
+Use the re modules findall function with the variable `IRCLOG`.
 EOD
 ,
 
@@ -211,6 +225,8 @@ Find the whole line where someone talks about `Kaffe`.
 Answer with the whole line.
 EOD
 ,
+
+"points" => 1,
 
 "answer" => function () {
 
@@ -234,6 +250,8 @@ Answer with all the lines.
 EOD
 ,
 
+"points" => 1,
+
 "answer" => function () {
 
     return ["09:47 -!- feeloor_ [~felix@c-83-233-58-114.cust.bredband2.com] has joined #db-o-webb", "09:49 -!- feeloor [~felix@c-83-233-58-114.cust.bredband2.com] has quit [Ping timeout: 240 seconds]", "19:10 -!- feeloor_ [~felix@c-83-233-58-114.cust.bredband2.com] has quit [Ping timeout: 240 seconds]", "19:11 -!- feeloor [~felix@c-83-233-58-114.cust.bredband2.com] has joined #db-o-webb"];
@@ -254,6 +272,8 @@ At what time interval did `Trekka12` have a `genombrott` with his `snakespel`?
 Answer with the time interval.
 EOD
 ,
+
+"points" => 2,
 
 "answer" => function () {
 
@@ -276,6 +296,8 @@ Find the two users who talk to `eline` about `git`/`Git`.
 Answer with the two usernames.
 EOD
 ,
+
+"points" => 1,
 
 "answer" => function () {
 
@@ -300,6 +322,8 @@ Answer with the usernames.
 EOD
 ,
 
+"points" => 2,
+
 "answer" => function () {
 
     return ["emil", "mos"];
@@ -322,6 +346,8 @@ Tip, marvin says when someone makes a forum post.
 Answer with the usernames.
 EOD
 ,
+
+"points" => 2,
 
 "answer" => function () {
 
@@ -370,6 +396,8 @@ One space between each word.
 EOD
 ,
 
+"points" => 1,
+
 "answer" => function () {
 
 	return "I know H.T.M.L. How To Meet Ladies";
@@ -392,6 +420,8 @@ and also add a space between them, so it becomes '123 abc 456 efg'
 EOD
 ,
 
+"points" => 1,
+
 "answer" => function () {
 
     return "123 abc 456 efg";
@@ -412,6 +442,8 @@ Use the sub function to remove the last three characters from the string
 EOD
 ,
 
+"points" => 1,
+
 "answer" => function () {
 
     return "Hello world!";
@@ -431,6 +463,8 @@ Use the sub function to remove the commented and the empty line from the string
 "#Did you back up the system?\\n\\nprint('hello world')".
 EOD
 ,
+
+"points" => 1,
 
 "answer" => function () {
 
@@ -453,6 +487,8 @@ For each person it should look like this:
 'Contact\nName: xx yy\nPhone number: 0000-00000\nAddress: 000 zzz zzz'.
 EOD
 ,
+
+"points" => 3,
 
 "answer" => function () {
 

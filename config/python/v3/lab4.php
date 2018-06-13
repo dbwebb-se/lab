ce60f106c246d629642d6364b28f2e8251711007
Emil Folino <emilfolino@gmail.com>
2018-06-13 11:20:38 +0200

    added 3 more exercises

--- config/python/v3/lab4.php
+++ config/python/v3/lab4.php
@@ -6,8 +6,25 @@
 include __DIR__ . "/../../random.php";
 
 
+$passwords = [
+    "Se3ret",
+    "mYsecretpassword",
+    "mYsecretpassw0rd",
+    "mYsecretPASSW0rd"
+];
 
+$passwordsRand = rand_int(0, count($passwords) - 1);
+$password = $passwords[$passwordsRand];
+$secondPassword = $passwords[($passwordsRand + 2)%(count($passwords))];
+$thirdPassword = $passwords[($passwordsRand + 3)%(count($passwords))];
 
+$stoicTexts = [
+    "Stoicism has just a few central teachings. It sets out to remind us of how unpredictable the world can be.",
+    "How brief our moment of life is. How to be steadfast, and strong, and in control of yourself.",
+    "And finally, that the source of our dissatisfaction lies in our impulsive dependency on our reflexive senses rather than logic."
+];
+$stoicTextsRand = rand_int(0, count($stoicTexts) - 1);
+$stoicText = $stoicTexts[$stoicTextsRand];
 
 /**
  * Titel and introduction to the lab.
@@ -58,16 +75,98 @@ In this lab we take another look at functions and we use modules to structure ou
 [
 
 "text" => "
+Create a function `valid_password` that takes one string argument. Check whether the argument is a valid password according to the following rules:
 
+* 8 characters or longer
+* Contains upper and lowercase letters
+* Contains a number
+
+The function should return True or False depending on whether the password is valid.
+
+Answer with the return value of the function when called with the string '$password'.
+
+Tip: Use built-in character functions `isupper()`, `islower()`, `idigit()`.
 ",
 "points" => 1,
-"answer" => function () {
-    return 1;
+"answer" => function () use ($password) {
+    return 1 === preg_match('~[0-9]~', $password) &&
+        1 === preg_match('~[A-Z]~', $password) &&
+        1 === preg_match('~[a-z]~', $password) &&
+        strlen($password) >= 8;
 },
 
 ],
 
 
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+Using the function `valid_password` answer with the return value of the function when called with the string '$secondPassword'.
+",
+"points" => 1,
+"answer" => function () use ($secondPassword) {
+    return 1 === preg_match('~[0-9]~', $secondPassword) &&
+        1 === preg_match('~[A-Z]~', $secondPassword) &&
+        1 === preg_match('~[a-z]~', $secondPassword) &&
+        strlen($secondPassword) >= 8;
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+Create a function `number_of_vowels` that takes one argument. The function returns the number of vowels (vokaler) in the given argument.
+
+Answer with the number of vowels in the following text:
+
+'$stoicText'
+",
+"points" => 1,
+"answer" => function () use ($stoicText) {
+    return preg_match_all('/[aeiou]/i', $stoicText, $matches);
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => "
+Create a function `analyze_password` that uses `valid_password` and `number_of_vowels`. The function should return whether or not a password is valid and how many vowels the given password contains concatenated to a string.
+
+Example: With the input value " . $passwords[0] . " the function should return the following string: '" . $passwords[0] . " is not a valid password and contains 2 vowels.'.
+
+With the input value " . $passwords[3] . " the function should return the following string: '" . $passwords[3] . " is a valid password and contains 4 vowels.'.
+
+Answer with the return value of the function `analyze_password` called with the following argument: $thirdPassword.
+",
+"points" => 1,
+"answer" => function () use ($thirdPassword) {
+    if (1 === preg_match('~[0-9]~', $thirdPassword) &&
+        1 === preg_match('~[A-Z]~', $thirdPassword) &&
+        1 === preg_match('~[a-z]~', $thirdPassword) &&
+        strlen($thirdPassword) >= 8) {
+            return "$thirdPassword is a valid password and contains " . preg_match_all('/[aeiou]/i', $thirdPassword, $matches) . " vowels.";
+    } else {
+        return "$thirdPassword is not a valid password and contains " . preg_match_all('/[aeiou]/i', $thirdPassword, $matches) . " vowels.";
+    }
+},
+
+],
 
 
 
